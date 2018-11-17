7d0ca8a8677229a16f4e965a9ad5bb9618897c0d
ash92596 <ashleigh.presley@faulkner.edu>
2018-11-17 09:55:15 -0600

    Step 3
    (Placed step 4 in wrong place so readding step 3)
    Save values as cookies

--- project1/make-cookies.php
+++ project1/make-cookies.php
@@ -1,76 +1,30 @@
 <?php
 
 
+    if( isset($_POST['theme']) )
+    {
+        // add 1 day to the current time for expiry time
+        $expiryTime = time()+60*60*24;
+        // create a persistent cookie
+        $themeName = "Theme";
+        $themeValue = $_POST['theme'];
+      
+        setcookie($themeName, $themeValue, $expiryTime);
+        
+        header("Location: chapter16-project1.php");
+    }
+
+  if( isset($_POST['philosopher']) )
+    {
+        // add 1 day to the current time for expiry time
+        $expiryTime = 0;
+        // create a session cookie
+        $philosopherName = "Philosopher";
+        $philosopherValue = $_POST['philosopher'];
+    
+        setcookie($philosopherName, $philosopherValue, $expiryTime);
+        
+        header("Location: chapter16-project1.php");
+    }
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Chapter 16</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.blue_grey-orange.min.css">
-
-    <link rel="stylesheet" href="css/styles.css">
-    
-    <script   src="https://code.jquery.com/jquery-1.7.2.min.js" ></script>
-    <script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
-  
-</head>
-
-<body>
-    
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
-            mdl-layout--fixed-header">
-            
-    <?php include 'includes/header.inc.php'; ?>
-    <?php include 'includes/left-nav.inc.php'; ?>
-    
-    <main class="mdl-layout__content mdl-color--grey-50">
-        <section class="page-content">
-
-            <div class="mdl-grid">
-
-
-              <div class="mdl-cell mdl-cell--12-col mdl-card  mdl-shadow--2dp">
-                <div class="mdl-card__title mdl-color--orange">
-                  <h2 class="mdl-card__title-text">Other Page</h2>
-                </div>
-                <div class="mdl-card__supporting-text">
-                    
-                    <?php
-                       if(isset($_COOKIE['Theme'])) {
-                              echo "The Persistent THEME cookie value is: " .$_COOKIE['Theme']. "<br><br>";
-                          } else {
-                              echo "Persistent THEME cookie not found.<br><br>";
-                          }        
-                            if(isset($_COOKIE['Philosopher'])) {
-                              echo "The Session PHILOSOPHER cookie value is : " .$_COOKIE['Philosopher'];
-                            } else {
-                                echo "Session PHILOSOPHER cookie not found.";
-                            }
-                    ?> 
-                    
-                    
-                </div>
-                <div class="mdl-card__actions mdl-card--border">
-                        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="remove-cookie.php">
-                          Remove Cookies
-                        </a>                    
-                </div>
-    
- 
-            </div>   
-           
-           
-            </div>  <!-- / mdl-grid -->    
-
-        </section>
-    </main>    
-</div>    <!-- / mdl-layout --> 
-          
-</body>
-</html>
