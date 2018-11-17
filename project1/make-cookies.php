95c8a1769a04f846993227ad7bd553090a412fb1
ash92596 <ashleigh.presley@faulkner.edu>
2018-11-17 09:42:55 -0600

    Step 3
    Saving values as persistent and session cookies

--- project1/make-cookies.php
+++ project1/make-cookies.php
@@ -1,1 +1,30 @@
+<?php
 
+
+    if( isset($_POST['theme']) )
+    {
+        // add 1 day to the current time for expiry time
+        $expiryTime = time()+60*60*24;
+        // create a persistent cookie
+        $themeName = "Theme";
+        $themeValue = $_POST['theme'];
+      
+        setcookie($themeName, $themeValue, $expiryTime);
+        
+        header("Location: chapter16-project1.php");
+    }
+
+  if( isset($_POST['philosopher']) )
+    {
+        // add 1 day to the current time for expiry time
+        $expiryTime = 0;
+        // create a session cookie
+        $philosopherName = "Philosopher";
+        $philosopherValue = $_POST['philosopher'];
+    
+        setcookie($philosopherName, $philosopherValue, $expiryTime);
+        
+        header("Location: chapter16-project1.php");
+    }
+
+?>
